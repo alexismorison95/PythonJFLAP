4a477f8f6bb3be32def369587502c0e6b4b91651
alexismorison95 <alexismorison83@gmail.com>
2020-03-28 19:11:30 -0300

    Codigo mas limpio

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from graficar import graficarAF
 from loadJFLAP import loadJFLAP
 from validar import generarDiccionario, validarCadena
 
+
 # Cargo el archivo JFLAP
-estados, inicial, final, transiciones = loadJFLAP(source="jflapArchivos/pares.jff", alfIsInt=False)
+estados, inicial, final, transiciones = loadJFLAP(source="jflapArchivos/prueba2.jff", alfIsInt=False)
 
 
 # Grafico el AF
 graficarAF(estados, inicial, final, transiciones)
 
+
 print()
+
 
 # Genero el diccionario del AF
 dicc = generarDiccionario(transiciones)
 
+
 # Valido una cadena
-cadena = "aaa"
+cadena = "11001010"
 
 res = validarCadena(cadena, dicc, inicial[0], final)
 
